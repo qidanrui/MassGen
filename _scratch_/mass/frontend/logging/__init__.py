fa330f112c78a6865da9ecc56fdea335b1e5b4d5
leezekun <lizekunlee@gmail.com>
2025-07-24 21:02:49 -0400

    clean code

--- _scratch_/mass/frontend/logging/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MASS Logging Components
-
-Provides real-time logging and monitoring capabilities for MASS coordination.
-"""
-
-from .realtime_logger import RealtimeLogger
-
-__all__ = ['RealtimeLogger']
